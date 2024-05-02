435426c96156861a9f57774fdd4383d5bbb4bd2e
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-02 11:07:22 +0700

    Add project files.

---
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -0,0 +1,22 @@
+// doublelinkedlist.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    int noMhs;
+    string name;
+    Node *next;
+    Node *prev;
+};
+
+
+int main()
+{
+    
+}
+
+
