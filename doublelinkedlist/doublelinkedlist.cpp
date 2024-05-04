b85614575800ba531e42841d31e863b43897b58c
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-04 12:36:09 +0700

    membuat conditional statement if untuk menyisipkan node baru di dalam list

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -21,4 +21,18 @@ void addNode() {
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;           //assign value to the data field of the new node
 
+    //insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs) {      //step 2: insert the new node
 
+        if (START != NULL && newNode->noMhs == START->noMhs) {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+        //If the list is empty, make the new node the START
+        newNode->next = START;          //step 3:make the new node point to the fisrt node
+        if (START != NULL) {
+            START->prev = newNode;      //step 4: make the new node point to the new node
+        }
+        newNode->prev - NULL;       //step 5: make the new node point to NULL
+        START = newNode;            //step 6: make the new node to the first node
+    }
