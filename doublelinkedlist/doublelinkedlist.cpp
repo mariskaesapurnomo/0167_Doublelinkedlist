60ab7b478f14cb52b537b7ea5d19420996438178
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-04 13:50:19 +0700

    perulangan dengan while dalam prosedur revtraverse

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -141,6 +141,13 @@ void revtraverse() {
     else {
         cout << "\nRecords in descending order of roll number are :" << endl;
         Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
     }
 }
 
